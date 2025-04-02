6e783840b635d4f966c5af81705828433e837c4f
nachele <59255243+nachele@users.noreply.github.com>
2025-04-02 09:48:40 +0200

    Add files via upload

--- Ajedrez.py
+++ Ajedrez.py
@@ -6,19 +6,34 @@ nfilas = 8
 ncolumnas = 8
 piezas = []
 direccionImagen = "piezas/"
+variable = 0
+seleccionadaIndicie = 0
 
 
 class Ficha:
-    def __init__(self,x,y, imagen):
+    def __init__(self,x,y, imagen,indice,color):
         self.nombre = imagen
+        self.indice = indice
+        self.color = color
         self.x = x
         self.y = y
         self.dibujo = pygame.image.load(imagen)
+        self.seleccionada = False
     def pintar(self):
         pantalla.blit(self.dibujo,(self.x,self.y))
     def mouseOn(self):
-        if posicionRaton()[0] > self.x and posicionRaton()[0] < self.x + 100 and posicionRaton()[1] > self.y and posicionRaton()[1] < self.y + 100 and teclaMouse()[0] == True:
-            print(self.nombre)
+        global seleccionadaIndicie
+        if posicionRaton()[0] > self.x and posicionRaton()[0] < self.x + 100 and posicionRaton()[1] > self.y and posicionRaton()[1] < self.y + 100 and teclaMouse()[0] == True and seleccionadaIndicie < 2:
+            self.seleccionada = True
+            seleccionadaIndicie += 1
+        elif teclaMouse()[0] == False:
+            self.seleccionada = False
+            seleccionadaIndicie = 0
+
+    def movimiento(self):
+        if self.seleccionada == True:
+            self.x = posicionRaton()[0] - 50
+            self.y = posicionRaton()[1] - 50
 
 def posicionRaton():
     return pygame.mouse.get_pos()
@@ -32,24 +47,30 @@ def teclaMouse():
 
 def PintarCuadro():
     #pygame.draw.rect(pantalla, colores[0], (100,100,100,100),0)
-    variable = 0
     for fila in range (nfilas):
         for columna in range (ncolumnas):
 
             pygame.draw.rect(pantalla, colores[(columna + fila)%2], (columna * 100, fila * 100, 100,100),0)
+            
+def crearPiezas():
+    #pygame.draw.rect(pantalla, colores[0], (100,100,100,100),0)
+    for fila in range (nfilas):
+        for columna in range (ncolumnas):
             if fila == 0:
-                piezas.append(Ficha(columna * 100, fila, direccionImagen + str(columna) + ".png"))
+                piezas.append(Ficha(columna * 100, fila, direccionImagen + str(columna) + ".png",columna, "blanca"))
             if fila == 1:
-                piezas.append(Ficha(columna * 100, fila * 100, direccionImagen + "8.png"))
+                piezas.append(Ficha(columna * 100, fila * 100, direccionImagen + "8.png", 8 + columna, "blanca"))
             if fila == 6:
-                piezas.append(Ficha(columna * 100, fila * 100, direccionImagen + "17.png"))
+                piezas.append(Ficha(columna * 100, fila * 100, direccionImagen + "17.png", 16 + columna, "negra"))
             if fila == 7:
-                piezas.append(Ficha(columna * 100, fila * 100, direccionImagen + str(columna + fila + 2) + ".png"))
-
+                piezas.append(Ficha(columna * 100, fila * 100, direccionImagen + str(columna + fila + 2) + ".png", 24 + columna,"negra"))
+crearPiezas()
 def PintarPiezas():
     for pieza in piezas:
-        pieza.pintar()
         pieza.mouseOn()
+        pieza.movimiento()
+        pieza.pintar()
+
 
             
 
